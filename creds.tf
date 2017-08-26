e119c112e9ade05e9247351bb943949fe625b6b2
yanir-home <yanir.dj@gmail.com>
2017-08-26 10:30:48 +0300

    ec2 simple instance

---
+++ creds.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    access_key = "AKIAII7X5G2BIOHKS4NQ"
+    secret_key = "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"
+    region = "us-east-1"
+}
---
+++ ec2.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "w1_security_group" {
+  name = "johndoe-sg"
+
+  description = "Test security group."
+
+  # This is fake VPC ID, you should put real one to make this configuration working
+  vpc_id = "vpc-59ea323c"
+}
+
+resource "aws_security_group_rule" "ssh_ingress_access" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = [ "0.0.0.0/0" ] 
+  security_group_id = "${aws_security_group.w1_security_group.id}"
+}
+
+resource "aws_security_group_rule" "egress_access" {
+  type = "egress"
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = "${aws_security_group.w1_security_group.id}"
+}
+
+
+resource "aws_instance" "w1_instance" {
+  instance_type = "t2.micro"
+  key_name = "user"
+  vpc_security_group_ids = [ "${aws_security_group.w1_security_group.id}" ]
+  associate_public_ip_address = true
+#  user_data = "${file("../../shared/user-data.txt")}"
+  tags {
+    Name = "w1-myinstance"
+  }
+
+  
+  ami = "ami-643b1972"
+  availability_zone = "us-east-1a"
+
+  # This is fake VPC subnet ID, please put real one to make this config working
+  subnet_id = "subnet-466a7000"
+}
